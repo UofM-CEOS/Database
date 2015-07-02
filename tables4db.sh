56c88422483ee57a246933337913edd64aa613f0
Sebastian Luque <spluque@gmail.com>
2015-07-02 13:25:14 -0500

    Rename programs and scripts, moving configuration files to subdirectory.

--- tables4db.sh
+++ tables4db.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 # Author: Sebastian Luque
 # Created: 2014-08-28T22:17:42+0000
-# Last-Updated: 2015-06-30T18:47:59+0000
-#           By: Sebastian P. Luque
+# Last-Updated: 2015-07-02T17:49:25+0000
+#           By: Sebastian Luque
 # 
 # Commentary: 
 #
@@ -23,25 +23,25 @@ LOGFILE2=${ROOTDIR}/Logs/met_log.csv
 LOGFILE3=${ROOTDIR}/Logs/complete_tower_log.csv
 AWKPATH=/usr/local/src/awk
 
-AWKPATH=${AWKPATH} ./nav4db_amundsen_flux_2010.awk ${NAV}/*.dat | \
+AWKPATH=${AWKPATH} ./nav4db.awk ${NAV}/*.dat | \
     awk -F, '!x[$1]++' > ${NAV}/NAV_all.csv
-./navproc4db_amundsen_flux.awk ${NAV_SHIP}/LEG_*/*.int | \
+./nav_proc4db.awk ${NAV_SHIP}/LEG_*/*.int | \
     awk -F, '!x[$1]++' > ${NAV_SHIP}/navproc_all.csv
-AWKPATH=${AWKPATH} ./met4db_amundsen_flux_2010.awk ${MET}/*.dat | \
+AWKPATH=${AWKPATH} ./met4db.awk ${MET}/*.dat | \
     awk -F, '!x[$1]++' > ${MET}/MET_all.csv
 ./underway4db.awk ${UNDERWAY}/*.txt | \
     awk '!x[$0]++' > ${UNDERWAY}/AMD_2010.csv
 ./underway4db_misc.awk ${UWTEMPERATURE}/* | \
     awk '!x[$0]++' > ${UNDERWAY}/AMD_water_temperature_2014.csv
-AWKPATH=${AWKPATH} ./rad4db_amundsen_flux_2010.awk ${RAD}/*.dat | \
+AWKPATH=${AWKPATH} ./rad4db.awk ${RAD}/*.dat | \
     awk '!x[$0]++' > ${RAD}/rad_all.csv
-AWKPATH=${AWKPATH} ./ec_avg_4db_amundsen_flux.awk ${FLUX_AVG}/*.dat | \
+AWKPATH=${AWKPATH} ./flux_avg4db.awk ${FLUX_AVG}/*.dat | \
     awk -F, '!x[$1]++' > ${FLUX_AVG}/flux_avg.csv
-AWKPATH=${AWKPATH} ./ec4db_amundsen_flux.awk ${FLUX}/*.dat | \
+AWKPATH=${AWKPATH} ./flux4db.awk ${FLUX}/*.dat | \
     awk -F, '!x[$1]++' > ${FLUX}/flux.csv
-./observer_log_4db.awk ${LOGFILE1} > $(dirname ${LOGFILE1})/closed_path_log_4db.csv
-./observer_log_4db.awk ${LOGFILE2} > $(dirname ${LOGFILE2})/metlog_4db.csv
-./observer_log_4db.awk ${LOGFILE3} > $(dirname ${LOGFILE3})/towerlog_4db.csv
+./observer_log4db.awk ${LOGFILE1} > $(dirname ${LOGFILE1})/closed_path_log_4db.csv
+./observer_log4db.awk ${LOGFILE2} > $(dirname ${LOGFILE2})/metlog_4db.csv
+./observer_log4db.awk ${LOGFILE3} > $(dirname ${LOGFILE3})/towerlog_4db.csv
 
 # Split based on program version (only EC in this year)
 ./split_progversion.awk -v PROGCOL=2 ${FLUX}/flux.csv
---
+++ underway4db_misc.awk
@@ -0,0 +1,48 @@
+#! /usr/bin/gawk -f
+# Author: Sebastian Luque
+# Created: 2014-11-04T21:35:25+0000
+# Last-Updated: 2015-06-30T18:48:26+0000
+#           By: Sebastian P. Luque
+# -------------------------------------------------------------------------
+# Commentary:
+#
+# See the pgloader configuration file for details.
+# -------------------------------------------------------------------------
+# Code:
+
+BEGIN {
+    FS=OFS=","
+    ncols=10			# number of columns
+    print "time,battery_voltage,logger_temperature,water_temperature",
+	"battery_voltage_sd,logger_temperature_sd,water_temperature_sd"
+}
+
+NF > ncols || $1 !~ /[[:digit:]]/ { next } # obviously garbage
+
+{				# skip if we have any garbage
+    for (i=1; i <= NF; i++) {
+	if ($i !~ /[[:alnum:]]/) next
+    }
+}
+
+{
+    hms=sprintf("%04d00", $4)
+    if (hms == "240000") {
+	$3++
+	hms="000000"
+    }
+    yyyymmdd=strftime("%F", mktime($2 " 1 " $3 " 0 0 0"))
+    dtime=sprintf("%s %02d:%02d:%02d", yyyymmdd,
+		  substr(hms, 1, 2),
+		  substr(hms, 3, 2), substr(hms, 5, 2))
+    printf "%s,", dtime		# ignore first useless column constant
+    # Print commas to get the same number of (ncols) fields always
+    for (i=5; i <= (ncols - 1); i++) { printf "%s,", $i }
+    printf "%s\n", $ncols
+}
+
+
+##_ + Emacs Local Variables
+# Local variables:
+# allout-layout: (-2 + : 0)
+# End:
