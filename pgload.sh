d2e75cb39c5fd63e9545d711b8d894bb04788b88
Sebastian Luque <spluque@gmail.com>
2015-07-02 20:29:54 -0500

    Update configuration file location.

--- pgload.sh
+++ pgload.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 # Author: Sebastian Luque
 # Created: 2014-02-05T22:44:42+0000
-# Last-Updated: 2015-07-01T13:21:38+0000
+# Last-Updated: 2015-07-01T13:43:08+0000
 #           By: Sebastian Luque
 # -------------------------------------------------------------------------
 # Commentary: 
@@ -19,31 +19,31 @@ Load tables
 pgloader -D /var/tmp/pgloader -S navigation_summary.log \
     -L /var/tmp/pgloader/navigation.log \
     --log-min-messages debug --client-min-messages warning \
-    navigation.pgload
+    pgloader/navigation.pgload
 pgloader -D /var/tmp/pgloader -S meteorology_summary.log \
     -L /var/tmp/pgloader/meteorology.log \
     --log-min-messages debug --client-min-messages warning \
-    meteorology.pgload
+    pgloader/meteorology.pgload
 pgloader -D /var/tmp/pgloader -S flux_avg_summary.log \
     -L /var/tmp/pgloader/flux_avg.log \
     --log-min-messages debug --client-min-messages warning \
-    flux_avg.pgload
+    pgloader/flux_avg.pgload
 pgloader -D /var/tmp/pgloader -S flux_summary.log \
     -L /var/tmp/pgloader/flux.log \
     --log-min-messages debug --client-min-messages warning \
-    flux.pgload
+    pgloader/flux.pgload
 pgloader -D /var/tmp/pgloader -S rad_summary.log \
     -L /var/tmp/pgloader/radiation.log \
     --log-min-messages debug --client-min-messages warning \
-    radiation.pgload
+    pgloader/radiation.pgload
 pgloader -D /var/tmp/pgloader -S UWpCO2_summary.log \
     -L /var/tmp/pgloader/uw_pCO2.log \
     --log-min-messages debug --client-min-messages warning \
-    uw_pCO2.pgload
+    pgloader/uw_pCO2.pgload
 pgloader -D /var/tmp/pgloader -S Logs_summary.log \
     -L /var/tmp/pgloader/logs.log \
     --log-min-messages debug --client-min-messages warning \
-    logs.pgload
+    pgloader/logs.pgload
 
 
 
