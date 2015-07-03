01c8246392ec9506266b3e90139f5607756b3eca
Sebastian Luque <spluque@gmail.com>
2015-07-03 17:02:18 -0500

    Revert to 2014 scripts.

--- tables4db.sh
+++ tables4db.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 # Author: Sebastian Luque
 # Created: 2014-08-28T22:17:42+0000
-# Last-Updated: 2015-06-30T18:47:59+0000
+# Last-Updated: 2015-07-03T21:56:30+0000
 #           By: Sebastian P. Luque
 # 
 # Commentary: 
@@ -10,42 +10,42 @@
 # -------------------------------------------------------------------------
 # Code:
 
-ROOTDIR=~/Data/ArcticNet/2010
-NAV=${ROOTDIR}/NAV
-NAV_SHIP=${NAV}/Ship/Processed
+ROOTDIR=~/Data/ArcticNet/2014
+POSMV=${ROOTDIR}/NAV/OMG/Renamed
+SHIPNAV=${ROOTDIR}/NAV/Tower
 MET=${ROOTDIR}/MET
 UNDERWAY=${ROOTDIR}/UW_pCO2
-RAD=${ROOTDIR}/RAD
-FLUX=${ROOTDIR}/Flux
-FLUX_AVG=${FLUX}/IRGA
-LOGFILE1=${ROOTDIR}/Logs/closed_path_log.csv
-LOGFILE2=${ROOTDIR}/Logs/met_log.csv
-LOGFILE3=${ROOTDIR}/Logs/complete_tower_log.csv
-AWKPATH=/usr/local/src/awk
+UWTEMPERATURE=${UNDERWAY}/Tsw
+RAD=${ROOTDIR}/RAD/STD
+EC_AVG=${ROOTDIR}/LI-7500A
+EC=${ROOTDIR}/EC
+LOGFILE1=${ROOTDIR}/closed_path_log_2014.csv
+LOGFILE2=${ROOTDIR}/met_log_2014.csv
 
-AWKPATH=${AWKPATH} ./nav4db_amundsen_flux_2010.awk ${NAV}/*.dat | \
-    awk -F, '!x[$1]++' > ${NAV}/NAV_all.csv
-./navproc4db_amundsen_flux.awk ${NAV_SHIP}/LEG_*/*.int | \
-    awk -F, '!x[$1]++' > ${NAV_SHIP}/navproc_all.csv
-AWKPATH=${AWKPATH} ./met4db_amundsen_flux_2010.awk ${MET}/*.dat | \
+./nmea2csv.awk ${POSMV}/*.log | \
+    awk -F, '!x[$1]++' > ${POSMV}/POSMV_all.csv
+./nav4db.awk ${SHIPNAV}/*.dat | \
+    awk -F, '!x[$1]++' > ${SHIPNAV}/NAV_all.csv
+./met4db.awk ${MET}/*.dat | \
     awk -F, '!x[$1]++' > ${MET}/MET_all.csv
-./underway4db.awk ${UNDERWAY}/*.txt | \
-    awk '!x[$0]++' > ${UNDERWAY}/AMD_2010.csv
+./underway4db.awk ${UNDERWAY}/*dat.txt | \
+    awk '!x[$0]++' > ${UNDERWAY}/AMD_2014.csv
 ./underway4db_misc.awk ${UWTEMPERATURE}/* | \
     awk '!x[$0]++' > ${UNDERWAY}/AMD_water_temperature_2014.csv
-AWKPATH=${AWKPATH} ./rad4db_amundsen_flux_2010.awk ${RAD}/*.dat | \
+./rad4db.awk ${RAD}/*.dat | \
     awk '!x[$0]++' > ${RAD}/rad_all.csv
-AWKPATH=${AWKPATH} ./ec_avg_4db_amundsen_flux.awk ${FLUX_AVG}/*.dat | \
-    awk -F, '!x[$1]++' > ${FLUX_AVG}/flux_avg.csv
-AWKPATH=${AWKPATH} ./ec4db_amundsen_flux.awk ${FLUX}/*.dat | \
-    awk -F, '!x[$1]++' > ${FLUX}/flux.csv
-./observer_log_4db.awk ${LOGFILE1} > $(dirname ${LOGFILE1})/closed_path_log_4db.csv
-./observer_log_4db.awk ${LOGFILE2} > $(dirname ${LOGFILE2})/metlog_4db.csv
-./observer_log_4db.awk ${LOGFILE3} > $(dirname ${LOGFILE3})/towerlog_4db.csv
+./flux_avg4db.awk ${EC_AVG}/*.dat
+./flux4db.awk ${EC}/*.dat
+./observer_log4db.awk ${LOGFILE1} > ${ROOTDIR}/obslog_4db.csv
+./observer_log4db.awk ${LOGFILE2} > ${ROOTDIR}/metlog_4db.csv
 
 # Split based on program version (only EC in this year)
-./split_progversion.awk -v PROGCOL=2 ${FLUX}/flux.csv
-./split_progversion.awk -v PROGCOL=2 ${FLUX_AVG}/flux_avg.csv
+./split_progversion.awk -v PROGCOL=3 ${EC}/motion.csv
+./split_progversion.awk -v PROGCOL=3 ${EC}/open_path_LI7500A_1.csv
+./split_progversion.awk -v PROGCOL=3 ${EC}/open_path_LI7500A_2.csv
+./split_progversion.awk -v PROGCOL=3 ${EC}/wind1_analog.csv
+./split_progversion.awk -v PROGCOL=3 ${EC}/wind1_serial.csv
+./split_progversion.awk -v PROGCOL=3 ${EC}/wind2_sdm.csv
 
 
 
