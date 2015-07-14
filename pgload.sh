bb0e4c7e4d7a78a456fb75dbcd944d923ed88270
Sebastian Luque <spluque@gmail.com>
2015-07-14 15:37:37 -0500

    Versions used to load Statoil 2015 data.

--- pgload.sh
+++ pgload.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 # Author: Sebastian Luque
 # Created: 2014-02-05T22:44:42+0000
-# Last-Updated: 2015-07-01T13:43:08+0000
-#           By: Sebastian Luque
+# Last-Updated: 2015-07-14T20:27:38+0000
+#           By: Sebastian P. Luque
 # -------------------------------------------------------------------------
 # Commentary: 
 #
@@ -14,36 +14,28 @@
 # Prepare data
 ./tables4db.sh
 
-Load tables
+# Load tables
 
-pgloader -D /var/tmp/pgloader -S navigation_summary.log \
-    -L /var/tmp/pgloader/navigation.log \
+pgloader -D /var/tmp/pgloader_statoil_2015 -S navigation_summary.log \
+    -L /var/tmp/pgloader_statoil_2015/navigation.log \
     --log-min-messages debug --client-min-messages warning \
     pgloader/navigation.pgload
-pgloader -D /var/tmp/pgloader -S meteorology_summary.log \
-    -L /var/tmp/pgloader/meteorology.log \
+pgloader -D /var/tmp/pgloader_statoil_2015 -S meteorology_summary.log \
+    -L /var/tmp/pgloader_statoil_2015/meteorology.log \
     --log-min-messages debug --client-min-messages warning \
     pgloader/meteorology.pgload
-pgloader -D /var/tmp/pgloader -S flux_avg_summary.log \
-    -L /var/tmp/pgloader/flux_avg.log \
-    --log-min-messages debug --client-min-messages warning \
-    pgloader/flux_avg.pgload
-pgloader -D /var/tmp/pgloader -S flux_summary.log \
-    -L /var/tmp/pgloader/flux.log \
-    --log-min-messages debug --client-min-messages warning \
-    pgloader/flux.pgload
-pgloader -D /var/tmp/pgloader -S rad_summary.log \
-    -L /var/tmp/pgloader/radiation.log \
+pgloader -D /var/tmp/pgloader_statoil_2015 -S rad_summary.log \
+    -L /var/tmp/pgloader_statoil_2015/radiation.log \
     --log-min-messages debug --client-min-messages warning \
     pgloader/radiation.pgload
-pgloader -D /var/tmp/pgloader -S UWpCO2_summary.log \
-    -L /var/tmp/pgloader/uw_pCO2.log \
-    --log-min-messages debug --client-min-messages warning \
-    pgloader/uw_pCO2.pgload
-pgloader -D /var/tmp/pgloader -S Logs_summary.log \
-    -L /var/tmp/pgloader/logs.log \
-    --log-min-messages debug --client-min-messages warning \
-    pgloader/logs.pgload
+# pgloader -D /var/tmp/pgloader -S UWpCO2_summary.log \
+#     -L /var/tmp/pgloader/uw_pCO2.log \
+#     --log-min-messages debug --client-min-messages warning \
+#     pgloader/uw_pCO2.pgload
+# pgloader -D /var/tmp/pgloader -S Logs_summary.log \
+#     -L /var/tmp/pgloader/logs.log \
+#     --log-min-messages debug --client-min-messages warning \
+#     pgloader/logs.pgload
 
 
 
--- tables4db.sh
+++ tables4db.sh
@@ -1,51 +1,51 @@
 #! /bin/sh
 # Author: Sebastian Luque
 # Created: 2014-08-28T22:17:42+0000
-# Last-Updated: 2015-06-30T18:47:59+0000
+# Last-Updated: 2015-07-14T19:43:07+0000
 #           By: Sebastian P. Luque
-# 
+# -------------------------------------------------------------------------
 # Commentary: 
 #
 # Prepare data for loading onto database.
 # -------------------------------------------------------------------------
 # Code:
 
-ROOTDIR=~/Data/ArcticNet/2010
-NAV=${ROOTDIR}/NAV
-NAV_SHIP=${NAV}/Ship/Processed
+ROOTDIR=~/Data/Statoil/2015
+NAV=${ROOTDIR}/NAV/Ship
 MET=${ROOTDIR}/MET
-UNDERWAY=${ROOTDIR}/UW_pCO2
+MET_AAVOS=${MET}/AAVOS
+# UNDERWAY=${ROOTDIR}/UW_pCO2
 RAD=${ROOTDIR}/RAD
-FLUX=${ROOTDIR}/Flux
-FLUX_AVG=${FLUX}/IRGA
+# FLUX=${ROOTDIR}/Flux
+# FLUX_AVG=${FLUX}/IRGA
 LOGFILE1=${ROOTDIR}/Logs/closed_path_log.csv
 LOGFILE2=${ROOTDIR}/Logs/met_log.csv
 LOGFILE3=${ROOTDIR}/Logs/complete_tower_log.csv
 AWKPATH=/usr/local/src/awk
 
-AWKPATH=${AWKPATH} ./nav4db_amundsen_flux_2010.awk ${NAV}/*.dat | \
-    awk -F, '!x[$1]++' > ${NAV}/NAV_all.csv
-./navproc4db_amundsen_flux.awk ${NAV_SHIP}/LEG_*/*.int | \
-    awk -F, '!x[$1]++' > ${NAV_SHIP}/navproc_all.csv
-AWKPATH=${AWKPATH} ./met4db_amundsen_flux_2010.awk ${MET}/*.dat | \
+./nav_proc4db.awk ${NAV}/NAV_*.int | \
+    awk -F, '!x[$1]++' > ${NAV}/navproc_all.csv
+AWKPATH=${AWKPATH} ./met4db.awk ${MET}/*.dat | \
     awk -F, '!x[$1]++' > ${MET}/MET_all.csv
-./underway4db.awk ${UNDERWAY}/*.txt | \
-    awk '!x[$0]++' > ${UNDERWAY}/AMD_2010.csv
-./underway4db_misc.awk ${UWTEMPERATURE}/* | \
-    awk '!x[$0]++' > ${UNDERWAY}/AMD_water_temperature_2014.csv
-AWKPATH=${AWKPATH} ./rad4db_amundsen_flux_2010.awk ${RAD}/*.dat | \
+AWKPATH=${AWKPATH} ./AAVOS_proc4db.awk ${MET_AAVOS}/*.int | \
+    awk -F, '!x[$1]++' > ${MET_AAVOS}/AAVOS_all.csv
+# ./underway4db.awk ${UNDERWAY}/*.txt | \
+#     awk '!x[$0]++' > ${UNDERWAY}/AMD_2010.csv
+# ./underway4db_misc.awk ${UWTEMPERATURE}/* | \
+#     awk '!x[$0]++' > ${UNDERWAY}/AMD_water_temperature_2014.csv
+AWKPATH=${AWKPATH} ./rad4db.awk ${RAD}/*.dat | \
     awk '!x[$0]++' > ${RAD}/rad_all.csv
-AWKPATH=${AWKPATH} ./ec_avg_4db_amundsen_flux.awk ${FLUX_AVG}/*.dat | \
-    awk -F, '!x[$1]++' > ${FLUX_AVG}/flux_avg.csv
-AWKPATH=${AWKPATH} ./ec4db_amundsen_flux.awk ${FLUX}/*.dat | \
-    awk -F, '!x[$1]++' > ${FLUX}/flux.csv
-./observer_log_4db.awk ${LOGFILE1} > $(dirname ${LOGFILE1})/closed_path_log_4db.csv
-./observer_log_4db.awk ${LOGFILE2} > $(dirname ${LOGFILE2})/metlog_4db.csv
-./observer_log_4db.awk ${LOGFILE3} > $(dirname ${LOGFILE3})/towerlog_4db.csv
+# AWKPATH=${AWKPATH} ./ec_avg_4db_amundsen_flux.awk ${FLUX_AVG}/*.dat | \
+#     awk -F, '!x[$1]++' > ${FLUX_AVG}/flux_avg.csv
+# AWKPATH=${AWKPATH} ./ec4db_amundsen_flux.awk ${FLUX}/*.dat | \
+#     awk -F, '!x[$1]++' > ${FLUX}/flux.csv
+# ./observer_log_4db.awk ${LOGFILE1} > $(dirname ${LOGFILE1})/closed_path_log_4db.csv
+# ./observer_log_4db.awk ${LOGFILE2} > $(dirname ${LOGFILE2})/metlog_4db.csv
+# ./observer_log_4db.awk ${LOGFILE3} > $(dirname ${LOGFILE3})/towerlog_4db.csv
 
-# Split based on program version (only EC in this year)
-./split_progversion.awk -v PROGCOL=2 ${FLUX}/flux.csv
-./split_progversion.awk -v PROGCOL=2 ${FLUX_AVG}/flux_avg.csv
+# # Split based on program version (only EC in this year)
+# ./split_progversion.awk -v PROGCOL=2 ${FLUX}/flux.csv
+# ./split_progversion.awk -v PROGCOL=2 ${FLUX_AVG}/flux_avg.csv
 
 
 
