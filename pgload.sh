90b3ba61dae5ec23b7c3465ddcee840a247a1d8e
Sebastian Luque <spluque@gmail.com>
2016-03-10 12:35:48 -0600

    Update paths and calls to reflect 2015 file structure.
    This is what was ultimately used to load 2015 data files onto database.

--- pgload.sh
+++ pgload.sh
@@ -13,22 +13,22 @@
 
 # Load tables
 
-pgloader -D /var/tmp/pgloader_statoil_2015 -S navigation_summary.log \
-    -L /var/tmp/pgloader_statoil_2015/navigation.log \
+pgloader -D /var/tmp/pgloader_AMD_2015 -S navigation_summary.log \
+    -L /var/tmp/pgloader_AMD_2015/navigation.log \
     --log-min-messages debug --client-min-messages warning \
     pgloader/navigation.pgload
-pgloader -D /var/tmp/pgloader_statoil_2015 -S meteorology_summary.log \
-    -L /var/tmp/pgloader_statoil_2015/meteorology.log \
+pgloader -D /var/tmp/pgloader_AMD_2015 -S meteorology_summary.log \
+    -L /var/tmp/pgloader_AMD_2015/meteorology.log \
     --log-min-messages debug --client-min-messages warning \
     pgloader/meteorology.pgload
-pgloader -D /var/tmp/pgloader_statoil_2015 -S rad_summary.log \
-    -L /var/tmp/pgloader_statoil_2015/radiation.log \
+pgloader -D /var/tmp/pgloader_AMD_2015 -S rad_summary.log \
+    -L /var/tmp/pgloader_AMD_2015/radiation.log \
     --log-min-messages debug --client-min-messages warning \
     pgloader/radiation.pgload
-# pgloader -D /var/tmp/pgloader -S UWpCO2_summary.log \
-#     -L /var/tmp/pgloader/uw_pCO2.log \
-#     --log-min-messages debug --client-min-messages warning \
-#     pgloader/uw_pCO2.pgload
+pgloader -D /var/tmp/pgloader_AMD_2015 -S UWpCO2_summary.log \
+    -L /var/tmp/pgloader_AMD_2015/uw_pCO2.log \
+    --log-min-messages debug --client-min-messages warning \
+    pgloader/uw_pCO2.pgload
 # pgloader -D /var/tmp/pgloader -S Logs_summary.log \
 #     -L /var/tmp/pgloader/logs.log \
 #     --log-min-messages debug --client-min-messages warning \
--- tables4db.sh
+++ tables4db.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 # Author: Sebastian Luque
 # Created: 2014-08-28T22:17:42+0000
-# Last-Updated: 2016-03-07T03:38:10+0000
-#           By: Sebastian Luque
+# Last-Updated: 2016-03-10T18:35:01+0000
+#           By: Sebastian P. Luque
 # -------------------------------------------------------------------------
 # Commentary: 
 #
@@ -23,7 +23,7 @@ MET_CEOS=${ROOTDIR}/MET/LoggerNet
 MET_AAVOS=${ROOTDIR}/MET/AAVOS
 UNDERWAY=${ROOTDIR}/UW_pCO2
 UWTEMPERATURE=${UNDERWAY}/Tsw
-TSG=${ROOTDIR}/TSG
+TSG=${UNDERWAY}/TSG
 RAD=${ROOTDIR}/RAD
 LOGFILE1=${ROOTDIR}/Logs/closed_path_log.csv
 LOGFILE2=${ROOTDIR}/Logs/met_log.csv
@@ -34,23 +34,21 @@ AWKPATH=/usr/local/src/awk
     awk -F, '!x[$1]++' > ${NAV}/NAV_LEG02-04.csv
 AWKPATH=${AWKPATH} ./met4db.awk ${MET_CEOS}/*.dat | \
     awk -F, '!x[$1]++' > ${MET_CEOS}/MET_all.csv
-AWKPATH=${AWKPATH} ./AAVOS_proc4db.awk ${MET_AAVOS}/*.int | \
-    awk -F, '!x[$1]++' > ${MET_AAVOS}/AAVOS_all.csv
+AWKPATH=${AWKPATH} ./AAVOS_proc4db.awk ${MET_AAVOS}/LEG_0[234]/*.csv | \
+    awk -F, '!x[$1]++' > ${MET_AAVOS}/AAVOS_LEG02-04.csv
 ./underway4db.awk ${UNDERWAY}/*.txt | \
     awk '!x[$0]++' > ${UNDERWAY}/UW_pCO2.csv
 ./underway4db_misc.awk ${UWTEMPERATURE}/* | \
     awk '!x[$0]++' > ${UNDERWAY}/UW_water_temperature.csv
 ./TSG4db.awk ${TSG}/LEG_0[234]/*.cnv | \
-    awk 'NR == 1 || !x[$0]++ && int(substr($1, 6, 2)) > 8' \
-	> ${TSG}/TSG.csv
+    awk 'NR == 1 || !x[$0]++' > ${TSG}/TSG.csv
 AWKPATH=${AWKPATH} ./rad4db.awk ${RAD}/*.dat | \
     awk '!x[$0]++' > ${RAD}/RAD_all.csv
-# ./observer_log_4db.awk ${LOGFILE2} > $(dirname ${LOGFILE2})/metlog_4db.csv
-# ./observer_log_4db.awk ${LOGFILE3} > $(dirname ${LOGFILE3})/towerlog_4db.csv
+./observer_log_4db.awk ${LOGFILE2} > $(dirname ${LOGFILE2})/metlog_4db.csv
+./observer_log_4db.awk ${LOGFILE3} > $(dirname ${LOGFILE3})/towerlog_4db.csv
 
-# # Split based on program version (only EC in this year)
-# ./split_progversion.awk -v PROGCOL=2 ${FLUX}/flux.csv
-# ./split_progversion.awk -v PROGCOL=2 ${FLUX_AVG}/flux_avg.csv
+# Split based on program version (only MET in this year)
+./split_progversion.awk -v PROGCOL=3 ${MET_CEOS}/MET_all.csv
 
 
 
