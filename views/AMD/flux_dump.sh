c54190b913631b71eff54973cda1d5f4dde7d500
Sebastian Luque <spluque@gmail.com>
2017-01-20 10:44:12 -0600

    Configure output of views

--- views/AMD/flux_dump.sh
+++ views/AMD/flux_dump.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 # Author: Sebastian Luque
 # Created: 2016-10-08T17:00:02+0000
-# Last-Updated: 2016-11-22T21:12:07+0000
-#           By: Sebastian P. Luque
+# Last-Updated: 2017-01-20T04:58:46+0000
+#           By: Sebastian Luque
 #
 # Commentary:
 #
@@ -15,17 +15,19 @@
 # -------------------------------------------------------------------------
 # Code:
 
-ROOTDIR=/mnt/CEOS_Tim/AMD/2016/FromDB
+ROOTDIR=/mnt/CEOS_Tim/AMD/2014/FromDB
 # Core low frequency views
-LFREQ1=lowfreq_1w20min_2016
+LFREQ1=lowfreq_1w20min_2014
 LFREQ1ODIR=${ROOTDIR}/LowFreq_1w20min
-LFREQ2=lowfreq_1w20min_2016_flags
+LFREQ2=lowfreq_1w20min_2014_flags
 LFREQ2ODIR=${ROOTDIR}/LowFreq_1w20min_flags
-LFREQ3=lowfreq_20min_fluxable_2016
-LFREQ3FILE=${ROOTDIR}/LowFreq_20min_fluxable/L3_2016.csv
+LFREQ3=lowfreq_20min_fluxable_2014
+LFREQ3FILE=${ROOTDIR}/LowFreq_20min_fluxable/L3_2014.csv
 # Core high frequency views
-HFREQ1=flux_10hz_2016
-HFREQ1ODIR=${ROOTDIR}/Flux_10hz
+HFREQ1=flux1_10hz_2014
+HFREQ1ODIR=${ROOTDIR}/Flux1_10hz
+HFREQ2=flux2_10hz_2014
+HFREQ2ODIR=${ROOTDIR}/Flux2_10hz
 # Program to split into daily files
 SPLITISO_PRG=$(realpath -e "$(dirname $0)"/../../split_YYYYMMDDHHMMSS.awk)
 SPLITYMD_PRG=$(realpath -e "$(dirname $0)"/../../split_YYYYMMDD.awk)
@@ -66,8 +68,8 @@ SELECT time_20min, time_study, longitude, longitude_avg, latitude, latitude_avg,
        ctd_conductivity, ctd_conductivity_avg, "ctd_O2_saturation",
        "ctd_O2_saturation_avg", "ctd_O2_concentration", "ctd_O2_concentration_avg",
        "uw_pH", "uw_pH_avg", uw_redox_potential, uw_redox_potential_avg,
-       temperature_external, temperature_external_avg, tsg_temperature,
-       tsg_temperature_avg, temperature_in, temperature_in_avg,
+       temperature_external, temperature_external_avg,
+       temperature_in, temperature_in_avg,
        bad_wind_direction_flag, very_bad_wind_direction_flag,
        bad_ice_flag, bad_atmospheric_pressure_flag, bad_anemometer_flag,
        bad_barometer_flag, bad_trh_sensor_flag, bad_ir_sensor_flag,
@@ -80,10 +82,17 @@ ORDER BY time_20min, time_study;
 \copy (SELECT * FROM lowfreq_1w20min) TO PROGRAM 'awk -v fprefix=L1 -f ${SPLITYMD_PRG} -' CSV
 \H
 \o colnames.html
-SELECT cols.column_name, col_description(cl.oid, cols.ordinal_position::INT)
-FROM pg_class cl, information_schema.columns cols
-WHERE cols.table_catalog='gases' AND cols.table_schema='amundsen_flux' AND
-cols.table_name = '${LFREQ1}' AND cols.table_name = cl.relname;
+SELECT DISTINCT ON (cols.ordinal_position, cols.column_name)
+    cols.column_name,
+    col_description(cl.oid, cols.ordinal_position::INT)
+FROM information_schema.columns cols
+    JOIN pg_class cl ON cols.table_name = cl.relname
+    JOIN pg_attribute pa ON cols.column_name = pa.attname
+WHERE pa.attrelid = 'lowfreq_1w20min'::regclass AND
+    cl.relname = '${LFREQ1}' AND
+    cols.table_catalog = 'gases' AND
+    cols.table_schema = 'amundsen_flux'
+ORDER BY cols.ordinal_position, cols.column_name;
 EOF
 psql -p5433 -f${TMPDIR}/lfreq1_dump.sql gases
 
@@ -120,8 +129,8 @@ SELECT time_20min, time_study, longitude, longitude_avg, latitude, latitude_avg,
        ctd_temperature_avg, ctd_conductivity, ctd_conductivity_avg, "ctd_O2_saturation",
        "ctd_O2_saturation_avg", "ctd_O2_concentration", "ctd_O2_concentration_avg",
        "uw_pH", "uw_pH_avg", uw_redox_potential, uw_redox_potential_avg,
-       temperature_external, temperature_external_avg, tsg_temperature,
-       tsg_temperature_avg, temperature_in, temperature_in_avg,
+       temperature_external, temperature_external_avg,
+       temperature_in, temperature_in_avg,
        bad_wind_direction_flag, very_bad_wind_direction_flag,
        bad_ice_flag, bad_atmospheric_pressure_flag, bad_anemometer_flag,
        bad_barometer_flag, bad_trh_sensor_flag, bad_ir_sensor_flag,
@@ -134,10 +143,17 @@ ORDER BY time_20min, time_study;
 \copy (SELECT * FROM lowfreq_1w20min_flags) TO PROGRAM 'awk -v fprefix=L2 -f ${SPLITYMD_PRG} -' CSV
 \H
 \o colnames.html
-SELECT cols.column_name, col_description(cl.oid, cols.ordinal_position::INT)
-FROM pg_class cl, information_schema.columns cols
-WHERE cols.table_catalog='gases' AND cols.table_schema='amundsen_flux' AND
-cols.table_name = '${LFREQ2}' AND cols.table_name = cl.relname;
+SELECT DISTINCT ON (cols.ordinal_position, cols.column_name)
+    cols.column_name,
+    col_description(cl.oid, cols.ordinal_position::INT)
+FROM information_schema.columns cols
+    JOIN pg_class cl ON cols.table_name = cl.relname
+    JOIN pg_attribute pa ON cols.column_name = pa.attname
+WHERE pa.attrelid = 'lowfreq_1w20min_flags'::regclass AND
+    cl.relname = '${LFREQ2}' AND
+    cols.table_catalog = 'gases' AND
+    cols.table_schema = 'amundsen_flux'
+ORDER BY cols.ordinal_position, cols.column_name;
 EOF
 psql -p5433 -f${TMPDIR}/lfreq2_dump.sql gases
 
@@ -148,7 +164,7 @@ SELECT time_20min, longitude, latitude, speed_over_ground, course_over_ground,
        relative_humidity, surface_temperature, wind_speed, wind_direction,
        true_wind_speed, true_wind_direction, "PAR", "K_down", "LW_down",
        nfluxable
-FROM amundsen_flux.lowfreq_20min_fluxable_2016;
+FROM amundsen_flux.${LFREQ3};
 \copy (SELECT * FROM lowfreq_20min_fluxable) TO '${LFREQ3FILE}' CSV HEADER
 EOF
 psql -p5433 -f${TMPDIR}/lfreq3_dump.sql gases
@@ -173,17 +189,63 @@ SELECT time_20min, time_study, longitude, latitude, speed_over_ground,
        "cp_H2O_absorptance", cp_pressure, cp_temperature, cp_temperature_in,
        cp_temperature_out, cp_temperature_block, cp_temperature_cell,
        "cp_CO2_signal_strength", "cp_H2O_signal_strength"
-  FROM amundsen_flux.flux_10hz_2016;
+  FROM amundsen_flux.${HFREQ1};
 \cd ${HFREQ1ODIR}
 \copy (SELECT * FROM flux_10hz) TO PROGRAM 'awk -v fprefix=EC -f ${SPLITISO_PRG} -' CSV
 \H
 \o colnames.html
-SELECT cols.column_name, col_description(cl.oid, cols.ordinal_position::INT)
-FROM pg_class cl, information_schema.columns cols
-WHERE cols.table_catalog='gases' AND cols.table_schema='amundsen_flux' AND
-cols.table_name = '${HFREQ1}' AND cols.table_name = cl.relname;
+SELECT DISTINCT ON (cols.ordinal_position, cols.column_name)
+    cols.column_name,
+    col_description(cl.oid, cols.ordinal_position::INT)
+FROM information_schema.columns cols
+    JOIN pg_class cl ON cols.table_name = cl.relname
+    JOIN pg_attribute pa ON cols.column_name = pa.attname
+WHERE pa.attrelid = 'flux_10hz'::regclass AND
+    cl.relname = '${HFREQ1}' AND
+    cols.table_catalog = 'gases' AND
+    cols.table_schema = 'amundsen_flux'
+ORDER BY cols.ordinal_position, cols.column_name;
 EOF
 psql -p5433 -f${TMPDIR}/hfreq1_dump.sql gases
+
+cat <<EOF > ${TMPDIR}/hfreq2_dump.sql
+CREATE OR REPLACE TEMPORARY VIEW flux_10hz AS
+SELECT time_20min, time_study, longitude, latitude, speed_over_ground,
+       course_over_ground, heading, pitch, roll, heave, atmospheric_pressure,
+       air_temperature, relative_humidity, surface_temperature, wind_speed,
+       wind_direction, true_wind_speed, true_wind_direction, "PAR",
+       "K_down", "LW_down", acceleration_x, acceleration_y, acceleration_z,
+       rate_x, rate_y, rate_z, wind_speed_u, wind_speed_v, wind_speed_w,
+       air_temperature_sonic, sound_speed, anemometer_status, "op_CO2_fraction",
+       "op_CO2_density", "op_CO2_absorptance", "op_H2O_fraction", "op_H2O_density",
+       "op_H2O_absorptance", op_pressure, op_temperature, op_temperature_base,
+       op_temperature_spar, op_temperature_bulb, op_cooler_voltage,
+       op_bandwidth, op_delay_interval, op_bad_chopper_wheel_temperature_flag,
+       op_bad_detector_temperature_flag, op_bad_optical_wheel_rate_flag,
+       op_bad_sync_flag, "op_CO2_signal_strength", op_analyzer_status,
+       cp_analyzer_status, "cp_CO2_fraction", "cp_CO2_density", "cp_CO2_dry_fraction",
+       "cp_CO2_absorptance", "cp_H2O_fraction", "cp_H2O_density", "cp_H2O_dry_fraction",
+       "cp_H2O_absorptance", cp_pressure, cp_temperature, cp_temperature_in,
+       cp_temperature_out, cp_temperature_block, cp_temperature_cell,
+       "cp_CO2_signal_strength", "cp_H2O_signal_strength"
+  FROM amundsen_flux.${HFREQ2};
+\cd ${HFREQ2ODIR}
+\copy (SELECT * FROM flux_10hz) TO PROGRAM 'awk -v fprefix=EC -f ${SPLITISO_PRG} -' CSV
+\H
+\o colnames.html
+SELECT DISTINCT ON (cols.ordinal_position, cols.column_name)
+    cols.column_name,
+    col_description(cl.oid, cols.ordinal_position::INT)
+FROM information_schema.columns cols
+    JOIN pg_class cl ON cols.table_name = cl.relname
+    JOIN pg_attribute pa ON cols.column_name = pa.attname
+WHERE pa.attrelid = 'flux_10hz'::regclass AND
+    cl.relname = '${HFREQ2}' AND
+    cols.table_catalog = 'gases' AND
+    cols.table_schema = 'amundsen_flux'
+ORDER BY cols.ordinal_position, cols.column_name;
+EOF
+psql -p5433 -f${TMPDIR}/hfreq2_dump.sql gases
 
 
 rm -rf ${TMPDIR}
--- views/AMD/lowfreq_dump.sh
+++ views/AMD/lowfreq_dump.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 # Author: Sebastian Luque
 # Created: 2016-10-08T17:00:02+0000
-# Last-Updated: 2016-10-23T01:05:01+0000
-#           By: Sebastian Luque
+# Last-Updated: 2017-01-18T18:38:31+0000
+#           By: Sebastian P. Luque
 #
 # Commentary:
 #
@@ -11,29 +11,76 @@
 # -------------------------------------------------------------------------
 # Code:
 
-ROOTDIR=/mnt/CEOS_Tim/AMD/2016/FromDB
+ROOTDIR=/mnt/CEOS_Tim/AMD/2014/FromDB
 # Core low frequency views
-NAV=navigation_1min_2016
+NAV=navigation_1min_2014
 NAVODIR=${ROOTDIR}/Navigation_1min
-MET=meteorology_ceos_1min_2016
+MET=meteorology_ceos_1min_2014
 METODIR=${ROOTDIR}/Meteorology_CEOS_1min
-RAD=radiation_1min_2016
+RAD=radiation_1min_2014
 RADODIR=${ROOTDIR}/RAD_1min
+UWPCO2=underway_1s_2014
+UWPCO2ODIR=${ROOTDIR}/UWpCO2_1s
 # Program to split into daily files
 SPLITISO_PRG=$(realpath -e "$(dirname $0)"/../../split_YYYYMMDDHHMMSS.awk)
 SPLITYMD_PRG=$(realpath -e "$(dirname $0)"/../../split_YYYYMMDD.awk)
 
 TMPDIR=$(mktemp -d -p /var/tmp)
 
+
+# Navigation
+cat <<EOF > ${TMPDIR}/nav_dump.sql
+CREATE OR REPLACE TEMPORARY VIEW navigation_1min AS
+SELECT time_study_1min, longitude_avg, longitude_sd, latitude_avg, latitude_sd,
+       speed_over_ground_avg, speed_over_ground_sd, course_over_ground_avg,
+       course_over_ground_sd, heading_avg, heading_sd, pitch_avg, roll_avg,
+       heave_avg
+FROM amundsen_flux.${NAV};
+\cd ${NAVODIR}
+\copy (SELECT * FROM navigation_1min) TO 'navigation_1min.csv' CSV HEADER
+EOF
+psql -p5433 -f${TMPDIR}/nav_dump.sql gases
+
+# Meteorology
+cat <<EOF > ${TMPDIR}/met_dump.sql
+CREATE OR REPLACE TEMPORARY VIEW meteorology_1min AS
+SELECT time_study, atmospheric_pressure, air_temperature, relative_humidity,
+       surface_temperature, wind_speed, wind_speed_sd, wind_direction,
+       wind_direction_sd, bad_barometer_flag, bad_trh_sensor_flag,
+       bad_ir_sensor_flag, bad_anemometer_flag, tower_down_flag
+FROM amundsen_flux.${MET};
+\cd ${METODIR}
+\copy (SELECT * FROM meteorology_1min) TO 'meterology_1min.csv' CSV HEADER
+EOF
+psql -p5433 -f${TMPDIR}/met_dump.sql gases
+
+# Radiation
 cat <<EOF > ${TMPDIR}/rad_dump.sql
 CREATE OR REPLACE TEMPORARY VIEW radiation_1min AS
 SELECT time_study, "PAR", "PAR_sd", "K_down", "K_down_sd", temperature_thermopile,
        temperature_thermopile_sd, temperature_case, temperature_case_sd,
        temperature_dome, temperature_dome_sd, "LW_down", "LW_down_sd"
-FROM amundsen_flux.radiation_1min_2016;
+FROM amundsen_flux.${RAD};
 \cd ${RADODIR}
 \copy (SELECT * FROM radiation_1min) TO 'radiation_1min.csv' CSV HEADER
 EOF
 psql -p5433 -f${TMPDIR}/rad_dump.sql gases
+
+# Underway
+cat <<EOF > ${TMPDIR}/uwpCO2_dump.sql
+CREATE OR REPLACE TEMPORARY VIEW underway_1s AS
+SELECT time_study, time_1min, equ_temperature, "uw_CO2_fraction",
+       "uw_H2O_fraction", uw_temperature_analyzer, uw_pressure_analyzer,
+       equ_pressure, "H2O_flow", air_flow_analyzer, condensation_equ,
+       ctd_pressure, ctd_temperature, ctd_conductivity, "ctd_O2_saturation",
+       "ctd_O2_concentration", temperature_external,
+       bad_ctd_flag, "bad_CO2_flag", "bad_H2O_flag", "bad_H2O_flow_flag",
+       bad_pressure_analyzer_flag, bad_temperature_analyzer_flag,
+       bad_equ_temperature_flag, bad_temperature_external_flag
+FROM amundsen_flux.${UWPCO2};
+\cd ${UWPCO2ODIR}
+\copy (SELECT * FROM underway_1s) TO 'underway_1s.csv' CSV HEADER
+EOF
+psql -p5433 -f${TMPDIR}/uwpCO2_dump.sql gases
 
 rm -rf ${TEMPDIR}
